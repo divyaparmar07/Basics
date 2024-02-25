795198acc0f5987155bd97bb8bcc48be4934159d
Divya Parmar <Divya.Parmar@einfochips.com>
2024-02-25 19:02:19 +0530

    Android

---
+++ app/src/main/java/com/example/basics/Practical.kt
@@ -0,0 +1,30 @@
+package com.example.basics
+
+fun main() {
+    var temp: Int = 10
+    println(temp)
+    temp = 20
+    println(temp)
+
+    var n1: Int = 10
+    var n2: Int = 20
+    var sum: Int = n1 + n2
+    println("sum is: $sum of $n1 and $n2")
+    println("sum is: ${sum}")
+
+    print("Enter your name")
+    var name = readLine()
+    println("Name: $name")
+
+    /*
+    when => like switch
+if and else
+
+looping
+for loop
+
+energy + confidence
+
+     */
+
+}
